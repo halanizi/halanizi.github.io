1b5464c1fba3378fe43e1335366ec7467a05736b
Hanna Alanizi <halanizi@ucsc.edu>
2022-05-25 16:07:12 -0700

    basic setup of components and global sass loader added

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,3 @@
+<template src="./component-templates/HeaderBar.html" />
+<!-- <script src="./component-scripts/HeaderBar.js"></script>
+<style lang="scss" src="../styles/_header-bar.scss"></style> -->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import HeaderBar from "./components/HeaderBar.vue"
 
-createApp(App).mount("#app");
+
+const app = createApp(App);
+app.component('HeaderBar', HeaderBar);
+app.mount("#app");
