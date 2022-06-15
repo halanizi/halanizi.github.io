77fcc959c10c703a29b78627162a46b7810c23c7
Hanna Alanizi <halanizi@ucsc.edu>
2022-06-14 20:01:22 -0700

    mobile view adjustments made

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,6 @@ module.exports = {
             @import "@/assets/styles/_variables.scss";
             @import "@/assets/styles/_headerbar.scss";
             @import "@/assets/styles/_homecomponent.scss";
-
           `,
       },
     },
